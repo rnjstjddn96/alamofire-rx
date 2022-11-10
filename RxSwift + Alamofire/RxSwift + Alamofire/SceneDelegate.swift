796d3ca5e008d81d77a334fbd57412c9aafed775
권성우 <swkwon@gwonseong-uui-MacBookPro.local>
2022-11-10 17:59:11 +0900

    config: storyboard removed

--- RxSwift + Alamofire/RxSwift + Alamofire/SceneDelegate.swift
+++ RxSwift + Alamofire/RxSwift + Alamofire/SceneDelegate.swift
@@ -16,8 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
         let rootViewController = ViewController()
-        self.window?.windowScene = windowScene
         self.window?.rootViewController = rootViewController
         
         window?.makeKeyAndVisible()
